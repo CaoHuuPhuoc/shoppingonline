ce86e6179ed8a5d8ad36223c4903642f25a9a8ad
CaoHuuPhuoc <conchobanh@gmail.com>
2024-07-17 22:16:44 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster.waxpfnp.mongodb.net',
+    DB_USER: 'db_user',
+    DB_PASS: 'db_pass',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '442',
+    JWT_EXPIRES: '341123123123123', // in milliseconds
+    EMAIL_USER: 'conchobanh@gmail.com', // gmail service
+    EMAIL_PASS: 'fqevqiuabtsorrwf'
+};
+module.exports = MyConstants;
